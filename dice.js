7f8a5b1eb46b37901c50a68bd67eb63dd28457ba
Brian Eng <brian@brianeng.com>
2016-10-13 15:07:44 -0500

    initial commit

---
+++ dice.js
@@ -0,0 +1,10 @@
+// e.g. getRandomInt(1, 6)
+var getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+$(function() {
+  // Your code here!
+  // Hint: .attr() is used to manipulate the value of an element's attribute
+  // Hint: dice images can be found at http://golearntocode.com/images/dice/number.png
+})
